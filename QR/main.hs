6015a99788fc3e4f6e9676bf6f71dd01706b8769
Jun SUMIDA <sjun@wondershake.com>
2014-05-11 13:29:30 +0900

    creates output file for first question

--- QR/main.hs
+++ QR/main.hs
@@ -1,2 +1,56 @@
-main :: IO ()
-main = putStrLn "Hello" >> putStrLn "World!!"
+import System.IO
+import Data.List.Split
+import qualified Data.Set as Set
+import Control.Monad
+
+main :: IO [()]
+main = do
+  input  <- readFile  "input.txt" 
+  -- rewrite splitOn " " with lambda?
+  let lines = map listToInt $ map (splitOn " ") $ splitOn "\n" $ init input 
+
+  let numberOfTestCases = head $ head lines
+  let testCases = tail lines 
+
+  processTestCases numberOfTestCases testCases 
+  -- can't do this cause `print $ processTestCases numberOfTestCases testCases` returns sth funny
+  --writeFile "output.txt" $ foldl (\acc str -> str ++ "\n") "" $ processTestCases numberOfTestCases testCases 
+
+listToInt :: [String] -> [Int]
+listToInt str = map read str
+
+parseLine :: Int -> [[Int]] -> [[Int]]
+parseLine index (x:xs) 
+  | index == 1 = x:xs
+  | otherwise = parseLine (index - 1) xs 
+
+tailLines :: Int -> [[Int]] -> [[Int]]
+tailLines index lines 
+  | index == 0 = lines
+  | index == 1 = tail lines 
+  | otherwise = tailLines (index - 1) (tail lines) 
+
+getIntersection :: [[Int]] -> Set.Set Int 
+getIntersection lines = do
+  let where_ans_is = head $ head lines
+  let first  = Set.fromList $ (parseLine where_ans_is (tail lines)) !! 0
+
+  let tails = tailLines 5 lines
+  let where_ans_is' = head $ head tails
+  let second = Set.fromList $ (parseLine where_ans_is' (tail tails)) !! 0
+
+  Set.intersection first second
+
+processTestCases :: Int -> [[Int]] -> IO [()] 
+processTestCases numberOfTestCases testCases = do
+  forM [0..numberOfTestCases - 1] $ \i -> do
+    appendFile "output.txt" $ "Case #" ++ (show i) ++ ": " ++ (setText $ getIntersection (tailLines (i * 10)  testCases)) ++ "\n"
+
+setText :: Set.Set Int -> String 
+setText intersection
+  | Set.size intersection == 0 = "Volunteer cheated!"
+  | Set.size intersection == 1 = selectOneFrom intersection 
+  | otherwise = "Bad magician!"
+
+selectOneFrom :: Set.Set Int -> String
+selectOneFrom set = show $ head $ Set.toList set
